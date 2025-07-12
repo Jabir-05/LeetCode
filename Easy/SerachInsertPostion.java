43195fddc7a10d5e32492e6570b48700b9a1ed93
Jabir-05 <knock5jabir@gmail.com>
2025-07-12 22:37:19 +0530

    esay

---
+++ Easy/SerachInsertPostion.java
@@ -0,0 +1,28 @@
+public class SerachInsertPostion {
+    public static int searchInsert(int[] nums, int target){
+        int start =0;
+        int end = nums.length-1;
+        while (start <= end) {
+            int mid = (start + end) /2;
+            if (nums[mid]== target) {
+                return mid;
+                
+            }
+            else if (nums[mid]> target) {
+                return mid-1;
+                
+            }
+            else{
+                return mid+1;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        System.out.println(searchInsert(nums,target)); 
+    }
+}
+    
+
