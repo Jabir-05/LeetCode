c8cd9923ff2e8d71bd76bb555390c55eae290574
Jabir-05 <knock5jabir@gmail.com>
2026-01-22 23:26:08 +0530

    Fabonacci Series

--- Revision/remove.java
+++ Revision/remove.java
@@ -13,7 +13,7 @@ public class remove {
 
     public static void main(String[] args) {
         int[] nums = {3, 2, 2, 3};
-        int val = 3;
+        int val = 9;
         System.out.println(removeElement(nums, val));
     } 
 }
