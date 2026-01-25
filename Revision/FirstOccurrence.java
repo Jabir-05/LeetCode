bda45210f1ce03370b000fb4a1d66b6b63f5a819
Jabir-05 <knock5jabir@gmail.com>
2026-01-25 19:05:56 +0530

    FirstOccurrence

---
+++ Revision/FirstOccurrence.java
@@ -0,0 +1,17 @@
+public class FirstOccurrence {
+
+    public static int firstOccurrence(String haystack, String needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "leetcode";
+        String needle = "leeto";
+        System.out.println(firstOccurrence(haystack, needle));
+    }
+}
